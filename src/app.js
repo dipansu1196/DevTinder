8e2eaf5195c90ced1e5f2aefb9a2a4ac1f20995c
Dipansu <dipansuchoudhary873@gmail.com>
2025-06-24 18:33:19 +0530

    understanding get,postand delete using postman

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express= require('express');
 const app = express();
+app.get("/user",(req, res) => {
+    res.send({firstname: "John", lastname: "Doe"});
+});
+app.post("/user",(req,res)=>{
+    res.send({message: "User created successfully!"});
+})
 app.use("/test",(req,res)=>{
 
     res.send("Hello World from Express!");
 })
+app.use("/",(req,res)=>{
+    res.send("Welcome to the Home Page!");
+});
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });

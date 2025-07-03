1472769224a4b8119b7850472bf3c31a03b6cb70
Dipansu <dipansuchoudhary873@gmail.com>
2025-07-03 16:13:20 +0530

    learn about expiresIn and expiresOn option in userAuth

--- src/app.js
+++ src/app.js
@@ -5,59 +5,52 @@ const app = express();
 const validator = require('validator');
 const User= require("./models/user");
 app.use(express.json());
-const cookieParser= require("cookie-parser") //covert json object
+const cookieParser= require("cookie-parser")
 const { validateSignupData } = require("./utils/validation");
 const jwt = require('jsonwebtoken');
 app.use(cookieParser());
+const {userAuth}= require("./middlewares/auth");
+
 app.post("/signup", async (req,res)=>{
-   
-   
     try{
-        // Validation Of data
-      validateSignupData(req.body);    
-
-   //Encrypt the password
-   const {firstName,lastName,email, password,age,skills}= req.body;
-   const passwordHash= await bcrypt.hash(password,10);
-   console.log(passwordHash);
-
-    // Create a new user instance
-    const user= new User({
-        firstName,
-        lastName,
-        email,
-        age,
-        skills,
-        password:passwordHash,
-    });
+        validateSignupData(req.body);    
+        const {firstName,lastName,email, password,age,skills}= req.body;
+        const passwordHash= await bcrypt.hash(password,10);
+        const user= new User({
+            firstName,
+            lastName,
+            email,
+            age,
+            skills,
+            password:passwordHash,
+        });
         await user.save();
         res.send("User added Successfully");
     }catch(err){
         console.log(err);
         res.status(400).send("Error adding user: " + err.message);
     }
-   } )
-app.get("/profile", async(req,res)=>{
- try{   const cookies= req.cookies;
-    const{token}= cookies;
-    if(!token){
-        return res.status(401).send("Unauthorized: No token provided");
-    }
-    const decodedMessage= await jwt.verify(token, "DEVTinder$790");
-    console.log(decodedMessage);
-    const{_id}= decodedMessage;
-    console.log("Logged In Use id is: ", _id);
-    console.log(cookies);
-    const user= await User.findById(_id);
-    if(!user){
-        return res.status(404).send("User not found");
-    }
-    res.send(user);}catch(err){
+})
+
+app.get("/profile", userAuth, async(req,res)=>{
+    try{
+        const cookies = req.cookies;
+        const {token} = cookies;
+        if(!token){
+            return res.status(401).send("Unauthorized: No token provided");
+        }
+        const decodedMessage = jwt.verify(token, "DEVTinder$790");
+        const {_id} = decodedMessage;
+        const user = await User.findById(_id);
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+        res.send(user);
+    }catch(err){
         res.status(400).send("Error fetching profile: " + err.message);
     }
-   
-
 })
+
 app.post("/login",async (req,res)=>{
     try{
         const {email,password}= req.body;
@@ -70,12 +63,7 @@ app.post("/login",async (req,res)=>{
         }
         const isPasswordValid= await bcrypt.compare(password, user.password);
         if(isPasswordValid){
-
-            //Create a JWT Token
             const token = jwt.sign({_id:user._id}, "DEVTinder$790");
-            console.log(token);
-
-            // Add the token to cookie and send the response back to the user
             res.cookie("token",token);
             res.send("Login successful");
         } else {
@@ -85,70 +73,69 @@ app.post("/login",async (req,res)=>{
         res.status(400).send("Error logging in: " + err.message);
     }
 })
-// find user by email
+
 app.get("/user",async (req,res)=>{
     const email= req.body.email;
- try{const user= await  User.find({email:email})
- if(user.length === 0){
-     return res.status(404).send("User not found");
- }
- res.send(user);
-}
-catch(err){
-    console.log(err);
-    res.status(400).send("Error finding user");
-}
+    try{
+        const user= await  User.find({email:email})
+        if(user.length === 0){
+            return res.status(404).send("User not found");
+        }
+        res.send(user);
+    }catch(err){
+        console.log(err);
+        res.status(400).send("Error finding user");
+    }
 })
+
 app.get("/feed",async (req,res)=>{
-try{
-  const users= await User.find({});
-  res.send(users);
-}catch(err){
-    res.status(400).send("Error fetching feed");
-}
+    try{
+        const users= await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("Error fetching feed");
+    }
 })
 
 app.delete("/user",async (req,res)=>{
-
-const userId= req.body.userId;
-try{
-    const user= await User.findByIdAndDelete(userId);
-    
-    res.send("User deleted successfully");
-}catch(err){
-    res.status(400).send("Error deleting user");
-}
+    const userId= req.body.userId;
+    try{
+        const user= await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(400).send("Error deleting user");
+    }
 })
 
-// Update user by email
 app.patch("/user/:userId",async(req,res)=>{
     const data =req.body;
     const userId= req.params?.userId;
-    
     try{
-         const ALLOWED_UPDATES= ["password","about","skills","gender","photoUrl"];
-    const isUpdateAllowed= Object.keys(data).every((update)=> ALLOWED_UPDATES.includes(update));
-    if(!isUpdateAllowed){
-        throw new Error("Update not allowed");
-    }
- const user=   await User.findByIdAndUpdate({
-        _id:userId
-    },data,{returnDocument: 'after',runValidators: true});
-  //  console.log(user);
-    res.send("User updated successfully");
+        const ALLOWED_UPDATES= ["password","about","skills","gender","photoUrl"];
+        const isUpdateAllowed= Object.keys(data).every((update)=> ALLOWED_UPDATES.includes(update));
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+        const user = await User.findByIdAndUpdate({
+            _id:userId
+        },data,{returnDocument: 'after',runValidators: true});
+        res.send("User updated successfully");
     }catch(err){
         res.status(400).send("Error updating user");
     }
-
 })
+
+app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
+    const user= req.user;
+    console.log("Sending a coonection request");
+    res.send(user.firstName+" is sending a relationship request to Poconut");
+})
+
 connectDB().then(()=>{
-console.log("Database connected successfully!");
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-});
+    console.log("Database connected successfully!");
+    app.listen(3000,()=>{
+        console.log('Server is running on port 3000');
+    });
 }).catch((err)=>{
     console.log(err);
 })
-
-
-
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,4 @@
- const adminAuth= (req,res,next)=>{
+const adminAuth= (req,res,next)=>{
     const token= "xyz123";
     const isAdminAuthorized= token=== "xyz123";
     if(isAdminAuthorized){
@@ -8,13 +8,27 @@
     }
 };
 
-const userAuth= (req,res,next)=>{
-    const token= "xyz123";
-    const isAdminAuthorized= token=== "xyz123";
-    if(isAdminAuthorized){
+const jwt= require("jsonwebtoken");
+const User= require("../models/user");
+
+const userAuth= async (req,res,next)=>{
+    try{ 
+        const {token}= req.cookies;
+        if(!token){
+            return res.status(401).send({message: "Unauthorized access"});
+        }
+        const decodedObj = jwt.verify(token, "DEVTinder$790",{expiresIn:"1d"});
+        const{_id}= decodedObj;
+        const user= await User.findById(_id);
+        if(!user){
+            return res.status(404).send({message: "User not found"});
+        }
+        req.user = user;
         next();
-    } else {
-        res.status(403).send({message: "Access denied"});
+    }catch(err){
+        console.error("Error in userAuth middleware:", err);
+        res.status(500).send({message: "Internal server error: " + err.message});
     }
 };
+
 module.exports = { adminAuth,userAuth};
